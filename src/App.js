f458ce3d28458a3d2e1c4b5912158688a018fce7
Jusi <megamahmutpower@gmail.com>
2022-09-19 00:39:40 +0300

    overlay cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import logo from './assets/img/logo.png'
-import plus from './assets/img/plus.svg'
-import sn1 from './assets/img/sneaker_1.jpg'
+import logo from './assets/img/logo.png';
+import plus from './assets/img/plus.svg';
+import sn1 from './assets/img/sneaker_1.jpg';
+import search from './assets/img/search__icon.svg';
+import remove from './assets/img/remove__item.svg';
 
 function App() {
   return (
     <div className="wrapper">
+      <div className='overlay'>
+        <div className='right__board'>
+          <h3 className='cart__title'>Корзина</h3>
+
+          <div className='cart__content'>
+
+            <div className='cart__item d-flex align-center'>
+              <img className='mr-20' width={70} height={70} src={sn1} alt='item' />
+              <div className='cart__item__desc mr-20'>
+                <p className='mb-5'>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <p>12 999 руб.</p>
+              </div>
+              <button className='remove__btn'>
+                <img src={remove} alt='remove' />
+              </button>
+            </div>
+
+            <div className='cart__item d-flex align-center'>
+              <img className='mr-20' width={70} height={70} src={sn1} alt='item' />
+              <div className='cart__item__desc mr-20'>
+                <p className='mb-5'>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <p>12 999 руб.</p>
+              </div>
+              <button className='remove__btn'>
+                <img src={remove} alt='remove' />
+              </button>
+            </div>
+
+          </div>
+
+          <ul className='cart__total'>
+            <li className='list__total d-flex'>
+              <span>Итого:</span>
+              <div className='line__total'></div>
+              <b>21 498 руб.</b>
+            </li>
+
+            <li className='list__total d-flex'>
+              <span>Налог 5%:</span>
+              <div className='line__total'></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <header className='d-flex justify-between align-center p-40'>
         <div className="header__left d-flex align-center">
           <img className='mr-15' width={40} height={40} src={logo} alt='Sneakers'/>
@@ -33,12 +81,28 @@ function App() {
       </header>
 
       <div className="content p-40">
-        <h1 className='mb-40'>Все кроссовки</h1>
+        <div className='d-flex justify-between align-center mb-40'>
+          <h1 className='title__main'>Все кроссовки</h1>
+          <div className='search-block d-flex'>
+            <img src={search} alt='search' />
+            <input placeholder='Поиск...' className='search__input'/>
+          </div>
+        </div>
         
-        <div className='d-flex'>
+        <div className='card__container d-flex'>
 
-        <div className='card'>
-            <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+          <div className='card'>
+            <div className='text-center'>
+              <div className='like'>
+                <div className='like__btn'>
+                  <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.5849 3.22311C14.3615 2.7098 14.0394 2.24464 13.6365 1.85368C13.2333 1.46155 12.758 1.14993 12.2363 0.935761C11.6954 0.712803 11.1152 0.59868 10.5295 0.600018C9.70772 0.600018 8.90596 0.823295 8.20921 1.24504C8.04253 1.34593 7.88418 1.45674 7.73416 1.57748C7.58414 1.45674 7.42579 1.34593 7.2591 1.24504C6.56236 0.823295 5.7606 0.600018 4.93884 0.600018C4.3471 0.600018 3.7737 0.712483 3.23198 0.935761C2.70858 1.15077 2.23686 1.46005 1.83181 1.85368C1.42843 2.2442 1.10619 2.70947 0.883373 3.22311C0.65168 3.75732 0.533333 4.32461 0.533333 4.90844C0.533333 5.45919 0.646679 6.0331 0.871705 6.61693C1.06006 7.10483 1.33009 7.61092 1.67513 8.12198C2.22186 8.93074 2.97361 9.77423 3.90705 10.6293C5.4539 12.0467 6.98574 13.0258 7.05075 13.0655L7.44579 13.3169C7.62081 13.4277 7.84584 13.4277 8.02086 13.3169L8.4159 13.0655C8.48091 13.0242 10.0111 12.0467 11.5596 10.6293C12.493 9.77423 13.2448 8.93074 13.7915 8.12198C14.1366 7.61092 14.4083 7.10483 14.5949 6.61693C14.82 6.0331 14.9333 5.45919 14.9333 4.90844C14.935 4.32461 14.8166 3.75732 14.5849 3.22311Z" fill="#c3c3c3"/>
+                  </svg>
+                </div>
+              </div>
+              <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+            </div>
+            
             <h5 className='card__title'>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className='card__bottom d-flex justify-beetwen align-center'>
               <div>
@@ -54,7 +118,17 @@ function App() {
           </div>
 
           <div className='card'>
-            <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+            <div className='text-center'>
+              <div className='like'>
+                <div className='like__btn'>
+                  <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.5849 3.22311C14.3615 2.7098 14.0394 2.24464 13.6365 1.85368C13.2333 1.46155 12.758 1.14993 12.2363 0.935761C11.6954 0.712803 11.1152 0.59868 10.5295 0.600018C9.70772 0.600018 8.90596 0.823295 8.20921 1.24504C8.04253 1.34593 7.88418 1.45674 7.73416 1.57748C7.58414 1.45674 7.42579 1.34593 7.2591 1.24504C6.56236 0.823295 5.7606 0.600018 4.93884 0.600018C4.3471 0.600018 3.7737 0.712483 3.23198 0.935761C2.70858 1.15077 2.23686 1.46005 1.83181 1.85368C1.42843 2.2442 1.10619 2.70947 0.883373 3.22311C0.65168 3.75732 0.533333 4.32461 0.533333 4.90844C0.533333 5.45919 0.646679 6.0331 0.871705 6.61693C1.06006 7.10483 1.33009 7.61092 1.67513 8.12198C2.22186 8.93074 2.97361 9.77423 3.90705 10.6293C5.4539 12.0467 6.98574 13.0258 7.05075 13.0655L7.44579 13.3169C7.62081 13.4277 7.84584 13.4277 8.02086 13.3169L8.4159 13.0655C8.48091 13.0242 10.0111 12.0467 11.5596 10.6293C12.493 9.77423 13.2448 8.93074 13.7915 8.12198C14.1366 7.61092 14.4083 7.10483 14.5949 6.61693C14.82 6.0331 14.9333 5.45919 14.9333 4.90844C14.935 4.32461 14.8166 3.75732 14.5849 3.22311Z" fill="#c3c3c3"/>
+                  </svg>
+                </div>
+              </div>
+              <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+            </div>
+            
             <h5 className='card__title'>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className='card__bottom d-flex justify-beetwen align-center'>
               <div>
@@ -70,7 +144,95 @@ function App() {
           </div>
 
           <div className='card'>
-            <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+            <div className='text-center'>
+              <div className='like'>
+                <div className='like__btn'>
+                  <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.5849 3.22311C14.3615 2.7098 14.0394 2.24464 13.6365 1.85368C13.2333 1.46155 12.758 1.14993 12.2363 0.935761C11.6954 0.712803 11.1152 0.59868 10.5295 0.600018C9.70772 0.600018 8.90596 0.823295 8.20921 1.24504C8.04253 1.34593 7.88418 1.45674 7.73416 1.57748C7.58414 1.45674 7.42579 1.34593 7.2591 1.24504C6.56236 0.823295 5.7606 0.600018 4.93884 0.600018C4.3471 0.600018 3.7737 0.712483 3.23198 0.935761C2.70858 1.15077 2.23686 1.46005 1.83181 1.85368C1.42843 2.2442 1.10619 2.70947 0.883373 3.22311C0.65168 3.75732 0.533333 4.32461 0.533333 4.90844C0.533333 5.45919 0.646679 6.0331 0.871705 6.61693C1.06006 7.10483 1.33009 7.61092 1.67513 8.12198C2.22186 8.93074 2.97361 9.77423 3.90705 10.6293C5.4539 12.0467 6.98574 13.0258 7.05075 13.0655L7.44579 13.3169C7.62081 13.4277 7.84584 13.4277 8.02086 13.3169L8.4159 13.0655C8.48091 13.0242 10.0111 12.0467 11.5596 10.6293C12.493 9.77423 13.2448 8.93074 13.7915 8.12198C14.1366 7.61092 14.4083 7.10483 14.5949 6.61693C14.82 6.0331 14.9333 5.45919 14.9333 4.90844C14.935 4.32461 14.8166 3.75732 14.5849 3.22311Z" fill="#c3c3c3"/>
+                  </svg>
+                </div>
+              </div>
+              <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+            </div>
+            
+            <h5 className='card__title'>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='card__bottom d-flex justify-beetwen align-center'>
+              <div>
+                <span className='price'>Цена:</span>
+                <p className='price__number'>12 999 руб.</p>
+              </div>
+              <button className='btn__buy'>
+                <svg width="11" height="11" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3"/>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <div className='text-center'>
+              <div className='like'>
+                <div className='like__btn'>
+                  <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.5849 3.22311C14.3615 2.7098 14.0394 2.24464 13.6365 1.85368C13.2333 1.46155 12.758 1.14993 12.2363 0.935761C11.6954 0.712803 11.1152 0.59868 10.5295 0.600018C9.70772 0.600018 8.90596 0.823295 8.20921 1.24504C8.04253 1.34593 7.88418 1.45674 7.73416 1.57748C7.58414 1.45674 7.42579 1.34593 7.2591 1.24504C6.56236 0.823295 5.7606 0.600018 4.93884 0.600018C4.3471 0.600018 3.7737 0.712483 3.23198 0.935761C2.70858 1.15077 2.23686 1.46005 1.83181 1.85368C1.42843 2.2442 1.10619 2.70947 0.883373 3.22311C0.65168 3.75732 0.533333 4.32461 0.533333 4.90844C0.533333 5.45919 0.646679 6.0331 0.871705 6.61693C1.06006 7.10483 1.33009 7.61092 1.67513 8.12198C2.22186 8.93074 2.97361 9.77423 3.90705 10.6293C5.4539 12.0467 6.98574 13.0258 7.05075 13.0655L7.44579 13.3169C7.62081 13.4277 7.84584 13.4277 8.02086 13.3169L8.4159 13.0655C8.48091 13.0242 10.0111 12.0467 11.5596 10.6293C12.493 9.77423 13.2448 8.93074 13.7915 8.12198C14.1366 7.61092 14.4083 7.10483 14.5949 6.61693C14.82 6.0331 14.9333 5.45919 14.9333 4.90844C14.935 4.32461 14.8166 3.75732 14.5849 3.22311Z" fill="#c3c3c3"/>
+                  </svg>
+                </div>
+              </div>
+              <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+            </div>
+            
+            <h5 className='card__title'>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='card__bottom d-flex justify-beetwen align-center'>
+              <div>
+                <span className='price'>Цена:</span>
+                <p className='price__number'>12 999 руб.</p>
+              </div>
+              <button className='btn__buy'>
+                <svg width="11" height="11" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3"/>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <div className='text-center'>
+              <div className='like'>
+                <div className='like__btn'>
+                  <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.5849 3.22311C14.3615 2.7098 14.0394 2.24464 13.6365 1.85368C13.2333 1.46155 12.758 1.14993 12.2363 0.935761C11.6954 0.712803 11.1152 0.59868 10.5295 0.600018C9.70772 0.600018 8.90596 0.823295 8.20921 1.24504C8.04253 1.34593 7.88418 1.45674 7.73416 1.57748C7.58414 1.45674 7.42579 1.34593 7.2591 1.24504C6.56236 0.823295 5.7606 0.600018 4.93884 0.600018C4.3471 0.600018 3.7737 0.712483 3.23198 0.935761C2.70858 1.15077 2.23686 1.46005 1.83181 1.85368C1.42843 2.2442 1.10619 2.70947 0.883373 3.22311C0.65168 3.75732 0.533333 4.32461 0.533333 4.90844C0.533333 5.45919 0.646679 6.0331 0.871705 6.61693C1.06006 7.10483 1.33009 7.61092 1.67513 8.12198C2.22186 8.93074 2.97361 9.77423 3.90705 10.6293C5.4539 12.0467 6.98574 13.0258 7.05075 13.0655L7.44579 13.3169C7.62081 13.4277 7.84584 13.4277 8.02086 13.3169L8.4159 13.0655C8.48091 13.0242 10.0111 12.0467 11.5596 10.6293C12.493 9.77423 13.2448 8.93074 13.7915 8.12198C14.1366 7.61092 14.4083 7.10483 14.5949 6.61693C14.82 6.0331 14.9333 5.45919 14.9333 4.90844C14.935 4.32461 14.8166 3.75732 14.5849 3.22311Z" fill="#c3c3c3"/>
+                  </svg>
+                </div>
+              </div>
+              <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+            </div>
+            
+            <h5 className='card__title'>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='card__bottom d-flex justify-beetwen align-center'>
+              <div>
+                <span className='price'>Цена:</span>
+                <p className='price__number'>12 999 руб.</p>
+              </div>
+              <button className='btn__buy'>
+                <svg width="11" height="11" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3"/>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <div className='text-center'>
+              <div className='like'>
+                <div className='like__btn'>
+                  <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.5849 3.22311C14.3615 2.7098 14.0394 2.24464 13.6365 1.85368C13.2333 1.46155 12.758 1.14993 12.2363 0.935761C11.6954 0.712803 11.1152 0.59868 10.5295 0.600018C9.70772 0.600018 8.90596 0.823295 8.20921 1.24504C8.04253 1.34593 7.88418 1.45674 7.73416 1.57748C7.58414 1.45674 7.42579 1.34593 7.2591 1.24504C6.56236 0.823295 5.7606 0.600018 4.93884 0.600018C4.3471 0.600018 3.7737 0.712483 3.23198 0.935761C2.70858 1.15077 2.23686 1.46005 1.83181 1.85368C1.42843 2.2442 1.10619 2.70947 0.883373 3.22311C0.65168 3.75732 0.533333 4.32461 0.533333 4.90844C0.533333 5.45919 0.646679 6.0331 0.871705 6.61693C1.06006 7.10483 1.33009 7.61092 1.67513 8.12198C2.22186 8.93074 2.97361 9.77423 3.90705 10.6293C5.4539 12.0467 6.98574 13.0258 7.05075 13.0655L7.44579 13.3169C7.62081 13.4277 7.84584 13.4277 8.02086 13.3169L8.4159 13.0655C8.48091 13.0242 10.0111 12.0467 11.5596 10.6293C12.493 9.77423 13.2448 8.93074 13.7915 8.12198C14.1366 7.61092 14.4083 7.10483 14.5949 6.61693C14.82 6.0331 14.9333 5.45919 14.9333 4.90844C14.935 4.32461 14.8166 3.75732 14.5849 3.22311Z" fill="#c3c3c3"/>
+                  </svg>
+                </div>
+              </div>
+              <img className='card__pic' width={133} height={112} src={sn1} alt='nike' />
+            </div>
+            
             <h5 className='card__title'>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className='card__bottom d-flex justify-beetwen align-center'>
               <div>
