b3e57a2070c345e06d1cff924e8b70d77f5469ca
Jusi <megamahmutpower@gmail.com>
2022-09-20 00:41:25 +0300

    started breaking down elements into components

---
+++ src/components/Cart.js
@@ -0,0 +1,59 @@
+import remove from '../assets/img/remove__item.svg';
+import arrow from '../assets/img/arrow.svg';
+import sn1 from '../assets/img/sneaker_1.jpg';
+
+function Cart() {
+    return (
+        <div className='overlay'>
+            <div className='right__board'>
+            <div className='d-flex'>          
+                <h3 className='cart__title'>Корзина</h3>
+                <img className='remove__btn ml-auto' src={remove} alt='remove' />
+            </div>
+
+            <div className='cart__content'>
+
+                <div className='cart__item d-flex align-center'>
+                <img className='mr-20' width={70} height={70} src={sn1} alt='item' />
+                <div className='cart__item__desc mr-20'>
+                    <p className='mb-5'>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                    <p>12 999 руб.</p>
+                </div>
+                <button className='remove__btn'>
+                    <img src={remove} alt='remove' />
+                </button>
+                </div>
+
+                <div className='cart__item d-flex align-center'>
+                <img className='mr-20' width={70} height={70} src={sn1} alt='item' />
+                <div className='cart__item__desc mr-20'>
+                    <p className='mb-5'>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                    <p>12 999 руб.</p>
+                </div>
+                <button className='remove__btn'>
+                    <img src={remove} alt='remove' />
+                </button>
+                </div>
+
+            </div>
+
+            <ul className='cart__total'>
+                <li className='list__total d-flex'>
+                <span>Итого:</span>
+                <div className='line__total'></div>
+                <b>21 498 руб.</b>
+                </li>
+
+                <li className='list__total d-flex'>
+                <span>Налог 5%:</span>
+                <div className='line__total'></div>
+                <b>1074 руб.</b>
+                </li>
+            </ul>
+            <button className='cart__buy'>Оформить заказ <img className='arrow__buy' src={arrow} alt='->' /></button>
+            </div>
+        </div>
+    );
+    }
+    
+export default Cart;
