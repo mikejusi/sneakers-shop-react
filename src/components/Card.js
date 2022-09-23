2dc2c2b93a7cb49fe99ef3dbaaffbb8879dd6fc2
Jusi <megamahmutpower@gmail.com>
2022-09-23 23:44:46 +0300

    *_*

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,8 @@
 function Card(props) {
+  const onClickCard = () => {
+    alert(22)
+  }
+
     return (
       <div className='card'>
         <div className='text-center'>
@@ -18,7 +22,7 @@ function Card(props) {
             <span className='price'>Цена:</span>
             <p className='price__number'>{props.price} руб.</p>
           </div>
-          <button className='btn__buy'>
+          <button className='btn__buy' onClick={onClickCard}>
             <svg width="11" height="11" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3"/>
             </svg>
